b050c7f67c3ab334aeb06daaf023500f7884365d
Jack Clark <chubz@Jacks-Mac-mini.local>
2017-04-18 15:55:25 -0500

    Initial Commit

---
+++ TotallyOriginalPasswordGenerator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TotallyOriginalPasswordGenerator
+//
+//  Created by Jack Clark on 4/18/17.
+//  Copyright © 2017 Jack Clark. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
